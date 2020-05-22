aa0574e3b8fb66bce1a3ce4efaed61f213432fd2
Daniel Amyakar <dan.rockit@gmail.com>
2020-05-22 01:27:08 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,7 +7,10 @@ class Artist
   def initialize(name)
     #binding.pry
     @name = name
+<<<<<<< HEAD
     @songs = []
+=======
+>>>>>>> 64bff620d1b1ee507aee277128c8b15f51a5321e
     save
   end
   
@@ -20,11 +23,19 @@ class Artist
   end
   
   def add_song(song)
+<<<<<<< HEAD
     @songs << song
   end
   
   def songs
     @songs
+=======
+    Song.artist = self
+  end
+  
+  def songs
+    Song.all.select{|song| song.artist == self.name}
+>>>>>>> 64bff620d1b1ee507aee277128c8b15f51a5321e
   end
   
   def self.find_or_create_by_name(artist_name)
@@ -36,7 +47,11 @@ class Artist
   end
       
   def print_songs
+<<<<<<< HEAD
     @songs.each {|song| puts song.name} 
+=======
+    Song.all.select{||song.artist == self.name}.name
+>>>>>>> 64bff620d1b1ee507aee277128c8b15f51a5321e
   end
   
 end
--- lib/song.rb
+++ lib/song.rb
@@ -24,9 +24,8 @@ class Song
   
   def self.new_by_filename(filename)
     file_data = filename.chomp('.mp3').split(' - ')
-    #binding.pry
+<<<<<<< HEAD
     song = self.new(file_data[1])
-    #song.name = 
     song.artist_name= file_data[0]
     song
   end
@@ -34,6 +33,16 @@ class Song
   def artist_name=(artist_name)
     #binding.pry
     self.artist = Artist.find_or_create_by_name(artist_name)
+=======
+    song = self.new
+    song.name = file_data[1]
+    song.artist_name(file_data[0])
+  end
+  
+  def artist_name=(artist_name)
+    Artist.find_or_create_by_name(artist_name)
+    self.artist(artist_name)
+>>>>>>> 64bff620d1b1ee507aee277128c8b15f51a5321e
   end
   
 end
